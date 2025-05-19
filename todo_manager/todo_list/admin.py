960b15db3644e28a7c67c73011ffc6e57f345fd0
Leshik1305 <realleshik1305@gmail.com>
2025-05-19 14:59:46 +0300

    add todo_list

---
+++ todo_manager/todo_list/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from todo_list.models import ToDoItem
+
+@admin.register(ToDoItem)
+class ToDoItemAdmin(admin.ModelAdmin):
+    list_display = "id", "title", "done"
+    list_display_links = "id", "title",
+    def __str__(self):
+        return self.title
